5c47c6c8f57d32a512adf7b221dcd380d0480765
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-05-27 02:50:34 +0700

    feat: add data types test
    Adds a test for data types.
    
    This test verifies that the following data types are declared:
    - string
    - number
    - boolean

---
+++ tests/data-type.test.ts
@@ -0,0 +1,11 @@
+describe('data type', function () {
+	it("must declared", function () {
+		let name: string = "irda islakhu afa"
+		let balance: number = 10_000_000
+		let isVIP: boolean = true
+
+		console.log(name)
+		console.log(balance)
+		console.log(isVIP)
+	})
+})
