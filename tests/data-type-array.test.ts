b433d8f195cbb795d4f5b09612ed8b9930b3a111
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-05-27 03:13:42 +0700

    feat(test): add test for array data type
    Adds a test to ensure that the array data type works as expected.

---
+++ tests/data-type-array.test.ts
@@ -0,0 +1,9 @@
+describe('array', function () {
+	it("must equals with js", function () {
+		const names: string[] = ["a", "b", "c"]
+		const values = [1, 2, 3]
+
+		console.log(names)
+		console.log(values)
+	})
+})
