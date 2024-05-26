07bdd9e757e648f7f2e676de0728a14821dc1782
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-05-27 03:25:11 +0700

    feat: support tupple and readonly array
    Add support for tupple and readonly array.
    
    Tupples are readonly arrays with explicit data types for each element and a fixed length of element.

--- tests/data-type-array.test.ts
+++ tests/data-type-array.test.ts
@@ -15,4 +15,16 @@ describe('array', function () {
 		// roArray[0] = 0 // error because read only
 		// roArray = [1, 2] // success
 	})
+
+	// tupple: readonly array with explicit data type for each element and fixed length of element
+	it("must support tupple", function () {
+		const roPersons: readonly [string, string, number] = ["irda", "islakhu afa", 22]
+		console.log(`ro persons: ${roPersons}`)
+		// roPersons[0] = "a" // error because read only
+		
+		// try tupple without readonly
+		const rwPersons: [string, string, number] = ["irda", "islakhu afa", 22]
+		console.log(`rw persons: ${rwPersons}`)
+		// rwPersons[0] = "a"// success because not read only
+})
 })
