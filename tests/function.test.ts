b89ab7cacf807c725774c292e510acfa2f157501
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-05-28 04:09:17 +0700

    feat: make function parameters optional
    This commit makes function parameters optional by default.
    
    This allows users to call functions without providing arguments, which can be useful in situations where the default values are appropriate.
    
    Additionally, this change simplifies the API of the function, making it easier to use.

--- tests/function.test.ts
+++ tests/function.test.ts
@@ -1,10 +1,10 @@
 describe('function', () => {
 	it('must support function', () => {
-		const sayHello = (name: string | null = "World"): string => `Hello ${name}`
+		const sayHello = (name: string = "World"): string => `Hello ${name}`
 		expect(sayHello()).toBe("Hello World")
 		expect(sayHello("irda")).toBe("Hello irda")
 
-		const printHello = (name: string | null = "World"): void => console.log(`Hello ${name}`)
+		const printHello = (name: string = "World"): void => console.log(`Hello ${name}`)
 		printHello()
 		printHello("irda")
 	})
