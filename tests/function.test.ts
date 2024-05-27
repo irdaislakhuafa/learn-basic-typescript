15d71c2bb412c8b7b5a90497eb495871a79e44ae
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-05-28 04:17:37 +0700

    feat: support optional parameter in sayHello
    Adds an optional lastName parameter to the sayHello function.
    
    This allows the function to be more flexible, as it can now be used to greet people with or without a last name.

--- tests/function.test.ts
+++ tests/function.test.ts
@@ -20,4 +20,17 @@ describe('function', () => {
 
 		expect(sum(1, 1, 1, 1, 1)).toBe(5)
 	})
+
+	it('must support optional parameter', () => {
+		const sayHello = (value: { firstName: string, lastName?: String }): string => {
+			if (value.lastName) {
+				return `Hello ${value.firstName} ${value.lastName}`
+			} else {
+				return `Hello ${value.firstName}`
+			}
+		}
+
+		expect(sayHello({ firstName: "a" })).toBe("Hello a")
+		expect(sayHello({ firstName: "a", lastName: "b" })).toBe("Hello a b")
+	})
 })
